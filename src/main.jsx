ce0bb3ec88e1a344c170b6c163e7caf568e77c65
Билол <126669638+bilol-anvarov@users.noreply.github.com>
2024-04-18 23:08:01 +0500

    upd adsensee

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,8 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    {/* Yandex.RTB  */}
+    <script>window.yaContextCb=window.yaContextCb||[]</script>
+    <script src="https://yandex.ru/ads/system/context.js" async></script>
   </React.StrictMode>,
 )
